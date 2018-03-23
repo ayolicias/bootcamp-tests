c3e5411326623a7923b988ee66b56ef2be26517b
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-03-23 09:52:37 +0200

    Update findItemsOver20.test.js

--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,10 +1,10 @@
 describe ('findItemsOver20', function(){
-  it('should count the itemListOver20', function(){
+  it('should count the itemListOver20 function', function(){
   assert.deepEqual(findItemsOver20([{name : 'apples', qty : 10},
     {name : 'bananas', qty : 27},
     {name : 'apples', qty : 3}]), [{'name' : 'bananas', 'qty' : 27}])
 });
-it('should count items less than20', function(){
+it('should count items less than20 for itemListOver20 function ', function(){
 assert.deepEqual(findItemsOver20([{name : 'apples', qty : 37},
   {name : 'bananas', qty : 10},
   {name : 'pears', qty : 12}]), [{'name' : 'apples', 'qty' : 37}])
