fa6404754e1df69236b5e8489916a57432d6a9f8
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-03-23 09:19:15 +0200

    Update countAllFromTown.test.js

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,5 +1,5 @@
 describe ('countAllFromTown', function(){
-it('counts number of registration for paarl', function(){
+it('counts number of registration for countAllFromTown function', function(){
 assert.equal(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341'), 3);
 });
 it('counts number of registration for town', function(){
