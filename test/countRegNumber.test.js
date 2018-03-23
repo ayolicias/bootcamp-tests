34c237f2de16c61200f2bdd999b5fc5f04179ac4
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-03-23 09:35:55 +0200

    Update countRegNumber.test.js

--- test/countRegNumber.test.js
+++ test/countRegNumber.test.js
@@ -1,9 +1,9 @@
 describe(' countRegNumber', function(){
-    it('counts regCount 5 regNumbers', function(){
+    it('counts regCount for regNumbers function', function(){
       assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328,CJ 812328,CJ 812328'), 5);
     });
 
-    it('check regNumbers', function(){
+    it('check regNumbers for regNumbers function', function(){
       assert.equal(countRegNumber('CY 523519,CJ 812328,CJ 812328,CJ 812328'), 4);
     });
     });
