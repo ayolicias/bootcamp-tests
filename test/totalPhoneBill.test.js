0335a94d465c1637c017ba48a6613332fc78dda3
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-03-23 09:56:57 +0200

    Update totalPhoneBill.test.js

--- test/totalPhoneBill.test.js
+++ test/totalPhoneBill.test.js
@@ -1,9 +1,9 @@
 describe ('totalPhoneBill', function(){
-  it('should count the totalPhoneBill', function(){
+  it('should count the totalPhoneBill function', function(){
   assert.equal(totalPhoneBill('call, sms, call, sms, sms' ),'R7.45');
   });
 
-  it('check type', function(){
+  it('check type for totalPhoneBill function', function(){
     assert.typeOf(totalPhoneBill('sms' ), 'string');
   });
 });
