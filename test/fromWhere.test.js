ac44988bdb620c9d51d5cfe2572eac2b1f8e6219
ayolicias <33415482+ayolicias@users.noreply.github.com>
2018-03-23 10:44:38 +0200

    Update fromWhere.test.js

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -4,6 +4,6 @@ describe ('fromWhere', function(){
   });
 
   it('check type for fromWhere function', function(){
-    assert.typeOf(fromWhere('CJ' ),'Paarl');
+    assert.typeOf(fromWhere('CJ' ),'paarl');
   });
 });
